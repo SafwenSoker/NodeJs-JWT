9e891c46ca008d222771719a84908ddb13254046
SafwenSoker <safwensoker1@gmail.com>
2022-01-25 23:25:07 +0100

    Authentication Middleware : Verifying JWT

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const {UnauthorizedError} = require('../errors')
+const jwt = require('jsonwebtoken')
+
+const authenticationMiddleware = async (req,res,next)=>{
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bearer')){
+        throw new UnauthorizedError('No token provided')
+    }
+    const token = authHeader.split(" ")[1]
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const {id, username} = decoded
+        req.user = {id,username}
+        next()
+    } catch (error) {
+        throw new UnauthorizedError('Not authorized to access this route')
+    }
+}
+module.exports= authenticationMiddleware

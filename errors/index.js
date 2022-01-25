a26aea802dfb4fd318ccad9be66206c88de7724f
SafwenSoker <safwensoker1@gmail.com>
2022-01-25 23:24:11 +0100

    Gathering all the errors

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UnauthorizedError = require('./unauthorized')
+
+module.exports = {
+    CustomAPIError,
+    BadRequest,
+    UnauthorizedError
+}
